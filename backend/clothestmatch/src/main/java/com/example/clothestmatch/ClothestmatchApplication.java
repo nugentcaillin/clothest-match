b6613079d8d4407955bbb450cb8375f5c90d6f3d
Caillin Nugent <passwords@qaervice.com>
2024-08-16 23:58:01 +1000

    Spring hello world

---
+++ backend/clothestmatch/src/main/java/com/example/clothestmatch/ClothestmatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothestmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothestmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothestmatchApplication.class, args);
+	}
+
+}
---
+++ backend/clothestmatch/src/test/java/com/example/clothestmatch/ClothestmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothestmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothestmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
